bfbcf7080b31ee2c84e95ba783677af3d88294ad
Patrick Arlt <patrick.arlt@gmail.com>
2017-08-11 11:07:10 -0700

    small tweaks for better parsing

--- src/index.js
+++ src/index.js
@@ -1,61 +1,64 @@
-import shell from 'shelljs';
-import path from 'path';
-import commitAnalyzer from '@semantic-release/commit-analyzer';
-import chalk from 'chalk';
-import buildCommit from 'cz-customizable/buildCommit';
-import autocomplete from 'inquirer-autocomplete-prompt';
-import Repository from 'lerna/lib/Repository';
-import PackageUtilities from 'lerna/lib/PackageUtilities';
+import shell from "shelljs";
+import path from "path";
+import commitAnalyzer from "@semantic-release/commit-analyzer";
+import chalk from "chalk";
+import buildCommit from "cz-customizable/buildCommit";
+import autocomplete from "inquirer-autocomplete-prompt";
+import Repository from "lerna/lib/Repository";
+import PackageUtilities from "lerna/lib/PackageUtilities";
 
-import makeDefaultQuestions from './make-default-questions';
-import autocompleteQuestions from './autocomplete-questions';
+import makeDefaultQuestions from "./make-default-questions";
+import autocompleteQuestions from "./autocomplete-questions";
 
-function getAllPackages () {
+function getAllPackages() {
   return PackageUtilities.getPackages(new Repository());
 }
 
-function getChangedPackages () {
-  const changedFiles = shell.exec('git diff --cached --name-only', {silent: true})
-    .stdout
-    .split('\n');
+function getChangedPackages() {
+  const changedFiles = shell
+    .exec("git diff --cached --name-only", { silent: true })
+    .stdout.split("\n");
 
   return getAllPackages()
-    .filter(function (pkg) {
-      const packagePrefix = path.relative('.', pkg.location) + path.sep;
+    .filter(function(pkg) {
+      const packagePrefix = path.relative(".", pkg.location) + path.sep;
       for (let changedFile of changedFiles) {
         if (changedFile.indexOf(packagePrefix) === 0) {
           return true;
         }
       }
     })
-    .map(function (pkg) {
-      return pkg.name
+    .map(function(pkg) {
+      return pkg.name;
     });
 }
 
-function makeAffectsLine (answers) {
+function makeAffectsLine(answers) {
   const selectedPackages = answers.packages;
   if (selectedPackages && selectedPackages.length) {
-    return `affects: ${selectedPackages.join(', ')}`;
+    return `AFFECTS PACKAGES:\r${selectedPackages.join("\r")}`;
   }
 }
 
-function getCommitTypeMessage (type) {
+function getCommitTypeMessage(type) {
   if (!type) {
-    return 'This commit does not indicate any release'
+    return "This commit does not indicate any release";
   }
   return {
-    patch: '🛠  This commit indicates a patch release (0.0.X)',
-    minor: '✨  This commit indicates a minor release (0.X.0)',
-    major: '💥  This commit indicates a major release (X.0.0)',
+    patch: "🛠  This commit indicates a patch release (0.0.X)",
+    minor: "✨  This commit indicates a minor release (0.X.0)",
+    major: "💥  This commit indicates a major release (X.0.0)"
   }[type];
 }
 
 function mergeQuestions(defaultQuestions, customQuestions) {
   const questions = [];
   defaultQuestions.forEach(question => {
-    const matchingCustomQuestions = customQuestions.filter(({ name: customQuestionName }) => (customQuestionName === question.name));
-    const customQuestion = matchingCustomQuestions.length > 0 && matchingCustomQuestions[0]
+    const matchingCustomQuestions = customQuestions.filter(
+      ({ name: customQuestionName }) => customQuestionName === question.name
+    );
+    const customQuestion =
+      matchingCustomQuestions.length > 0 && matchingCustomQuestions[0];
     questions.push(customQuestion || question);
   });
   return questions;
@@ -63,40 +66,47 @@ function mergeQuestions(defaultQuestions, customQuestions) {
 
 function makePrompter(makeCustomQuestions = () => []) {
   return function(cz, commit) {
-    const allPackages = getAllPackages().map((pkg) => pkg.name);
+    const allPackages = getAllPackages().map(pkg => pkg.name);
     const changedPackages = getChangedPackages();
 
     const defaultQuestions = makeDefaultQuestions(allPackages, changedPackages);
     const customQuestions = makeCustomQuestions(allPackages, changedPackages);
     const questions = mergeQuestions(defaultQuestions, customQuestions);
 
-    console.log('\n\nLine 1 will be cropped at 100 characters. All other lines will be wrapped after 100 characters.\n');
+    console.log(
+      "\n\nLine 1 will be cropped at 100 characters. All other lines will be wrapped after 100 characters.\n"
+    );
 
-    cz.registerPrompt('autocomplete', autocomplete);
-    cz.prompt(
-      autocompleteQuestions(questions)
-    ).then((answers) => {
+    cz.registerPrompt("autocomplete", autocomplete);
+    cz.prompt(autocompleteQuestions(questions)).then(answers => {
       const affectsLine = makeAffectsLine(answers);
+      console.log(affectsLine);
       if (affectsLine) {
-        answers.body = `${affectsLine}\n` + answers.body;
+        answers.body = `${answers.body}\n\n${affectsLine}`;
       }
       const message = buildCommit(answers);
-      const type = commitAnalyzer({}, {
-        commits: [{
-          hash: '',
-          message,
-        }],
-      }, (err, type) => {
-        console.log(chalk.green(`\n${getCommitTypeMessage(type)}\n`));
-        console.log('\n\nCommit message:');
-        console.log(chalk.blue(`\n\n${message}\n`));
-        commit(message)
-      });
+      const type = commitAnalyzer(
+        {},
+        {
+          commits: [
+            {
+              hash: "",
+              message
+            }
+          ]
+        },
+        (err, type) => {
+          console.log(chalk.green(`\n${getCommitTypeMessage(type)}\n`));
+          console.log("\n\nCommit message:");
+          console.log(chalk.blue(`\n\n${message}\n`));
+          commit(message);
+        }
+      );
     });
-  }
+  };
 }
 
 module.exports = {
   prompter: makePrompter(),
-  makePrompter: makePrompter,
+  makePrompter: makePrompter
 };
